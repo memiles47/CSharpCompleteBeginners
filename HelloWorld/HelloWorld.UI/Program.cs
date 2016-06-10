8edcd57f1c645162e0f5b05552232099422607f5
Michael E Miles <michael@miles-me.net>
2016-06-10 06:39:30 -0500

    Obligitory Hello World Application

---
+++ HelloWorld/HelloWorld.UI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld.UI
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+}
