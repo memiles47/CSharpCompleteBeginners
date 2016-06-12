c3736cebc2a68fe3185fed2b71616d44a5ecbe36
Michael E Miles <michael@miles-me.net>
2016-06-12 14:46:30 -0500

    Hello Name program, asking for your name and addressing you as "Hello" your name.

---
+++ HelloName/HelloName/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"\nHello, {name}");
+            Console.ReadLine();
+        }
+    }
+}
