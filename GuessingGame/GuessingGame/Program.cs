e00bc4299c021cba507683ecf38a4ade68360655
Michael E Miles <michael@miles-me.net>
2016-06-14 12:24:16 -0500

    Two lines added to calculate and show number of guesses

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -9,19 +9,21 @@ namespace GuessingGame
             int guess;
             var randomNumber = new Random();
             var answer = randomNumber.Next(1, 101);
+            var numberOfGuesses = 0;
 
             do
             {
                 Console.Write("Guess a number between 1 and 100: ");
                 int tempParse;
                 guess = int.TryParse(Console.ReadLine(), out tempParse)? tempParse : 0;
+                numberOfGuesses++;
 
                 if (guess != answer)
                     Console.WriteLine($"Your guess is {(guess < answer ? "Low" : "High")}"); 
 
             } while (answer != guess);
 
-            Console.WriteLine($"You guessed the answer of {guess} correctly");
+            Console.WriteLine($"You guessed the answer of {guess} correctly in {numberOfGuesses} guesses.");
             Console.ReadLine();
         }
     }
