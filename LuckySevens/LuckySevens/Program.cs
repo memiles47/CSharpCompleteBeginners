80e348d20a6e774c4684783cad652c53ee9ea372
Michael E Miles <michael@miles-me.net>
2016-06-14 13:53:14 -0500

    Code here was very much exactly letter for letter exactly the same as demonstration

---
+++ LuckySevens/LuckySevens/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LuckySevens
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dieRoll = new Random();
+            int die1, die2, rollNumber = 0;
+
+            for (int i = 1; i < 101; i++)
+            {
+                die1 = dieRoll.Next(1, 7);
+                die2 = dieRoll.Next(1, 7);
+
+                if (die1 + die2 == 7)
+                    rollNumber++;
+            }
+
+            Console.WriteLine($"Rolled seven {rollNumber} time(s)");
+            Console.ReadLine();
+        }
+    }
+}
