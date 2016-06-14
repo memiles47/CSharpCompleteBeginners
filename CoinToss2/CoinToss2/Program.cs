71e7b4b38f87dff40d2a69045f068e79388e6abc
Michael E Miles <michael@miles-me.net>
2016-06-14 03:11:58 -0500

    Create a cointoss two by Refactoring the original creating less code.

---
+++ CoinToss2/CoinToss2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CoinToss2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var coin = new Random();
+
+            Console.Write("Enter your guess, Heads or Tales \"H\" or \"T\": ");
+            var guess = Console.ReadLine();
+
+            var coinToss = coin.Next(0, 2);
+
+            if (guess?.ToLower() == "h" && coinToss == 1)
+                Console.WriteLine("The coin toss was Heads, You win!");
+
+            else if (guess?.ToLower() == "t" && coinToss == 0)
+                Console.WriteLine("THe coin toss was Tales, You win!");
+
+            else
+                Console.WriteLine(coinToss == 0 ? "Coin toss was tales, you lose!" : "Coin toss was heads, you lose!");
+
+            Console.ReadLine();
+        }
+    }
+}
