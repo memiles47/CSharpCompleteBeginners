f0400b3fc8b7e30eb5981e2744bbc251723f6d43
Michael E Miles <michael@miles-me.net>
2016-06-15 05:03:54 -0500

    Complete stopwatch

--- StopwatchDemo/StopwatchDemo/Program.cs
+++ StopwatchDemo/StopwatchDemo/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace StopwatchDemo
+﻿using System;
+
+namespace StopwatchDemo
 {
     class Program
     {
         static void Main()
         {
+            var timer = new Stopwatch();
 
+            Console.WriteLine("Press <Enter> to start the timeer");
+            Console.ReadLine();
+            timer.Start();
+            
+            Console.WriteLine("Press <Enter> to stop the timer");
+            Console.ReadLine();
+            timer.Stop();
+
+            Console.ReadLine();
         }
     }
 }
--- StopwatchDemo/StopwatchDemo/Stopwatch.cs
+++ StopwatchDemo/StopwatchDemo/Stopwatch.cs
@@ -1,7 +1,20 @@
-﻿namespace StopwatchDemo
+﻿using System;
+
+namespace StopwatchDemo
 {
     class Stopwatch
     {
-        
+        private DateTime StartTime { get; set; }
+
+        public void Start()
+        {
+            StartTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            var time = DateTime.Now - StartTime;
+            Console.WriteLine($"Elapsed Time: {time.Seconds} seconds, {time.Milliseconds} millisecoonds");
+        }
     }
 }
