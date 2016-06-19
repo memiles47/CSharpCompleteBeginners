f183398a215d5ad69d23bb786cfce730ac3fcf10
Michael E Miles <michael@miles-me.net>
2016-06-19 09:53:39 -0500

    My version of Rock Paper Scissors

--- RockPaperScissors/RockPaperScissors/PlayGame.cs
+++ RockPaperScissors/RockPaperScissors/PlayGame.cs
@@ -1,7 +1,46 @@
+using System;
+
 namespace RockPaperScissors
 {
     class PlayGame
     {
+        public void Rock(int computerGuess)
+        {
+            if (computerGuess == 1)
+                Console.WriteLine("Rock -vs- Rock: Tie game");
+            else if (computerGuess == 2)
+                Console.WriteLine("Paper covers Rock: Computer wins!");
+            else
+                Console.WriteLine("Rock beats Scissors: You win!");
 
+            Console.Write("Press enter to continue");
+            Console.ReadLine();
+        }
+
+        public void Paper(int computerGuess)
+        {
+            if (computerGuess == 1)
+                Console.WriteLine("Paper covers Rock: You win!");
+            else if (computerGuess == 2)
+                Console.WriteLine("Paper -vs- Paper: Tie game");
+            else
+                Console.WriteLine("Scisors cut Paper: Computer Wins!");
+
+            Console.Write("Press enter to continue");
+            Console.ReadLine();
+        }
+
+        public void Scissors(int computerGuess)
+        {
+            if (computerGuess == 1)
+                Console.WriteLine("Rock beats Scissors: Computer Wins!");
+            else if (computerGuess == 2)
+                Console.WriteLine("Scissors cut Paper: You Win!");
+            else
+                Console.WriteLine("Scissors -vs- Scissors: Tie game");
+
+            Console.Write("Press enter to continue");
+            Console.ReadLine();
+        }
     }
 }
--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -6,10 +6,33 @@ namespace RockPaperScissors
     {
         static void Main()
         {
-            Console.Write("Enter \"Rock\", \"Paper\" or \"Scissors\": ");
-            var guess = Console.ReadLine().ToLower();
+            var game = new PlayGame();
+            var userGuess = string.Empty;
+            var rps = new Random();
 
-            Console.ReadLine();
+            while (userGuess != "quit")
+            {
+                Console.Clear();
+                Console.Write("Enter \"Rock\", \"Paper\", \"Scissors\" or \"Quit\": ");
+                userGuess = Console.ReadLine()?.ToLower();
+
+                if (userGuess != "rock" && userGuess != "paper" && userGuess != "scissors" && userGuess != "quit")
+                {
+                    Console.WriteLine("Must enter Rock, Paper, Scissors or Quit");
+                    Console.Write("Press Enter to continue");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                var computerGuess = rps.Next(1,4);
+
+                if (userGuess == "rock")
+                    game.Rock(computerGuess);
+                else if (userGuess == "paper")
+                    game.Paper(computerGuess);
+                else if (userGuess == "scissors")
+                    game.Scissors(computerGuess);
+            }
         }
     }
 }
