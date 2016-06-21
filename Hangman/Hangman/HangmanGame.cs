4e892ff8b19755f9500884ef288f7fe221503266
Michael E Miles <michael@miles-me.net>
2016-06-21 14:19:09 -0500

    Skeleton for Hangman game

---
+++ Hangman/Hangman/HangmanGame.cs
@@ -0,0 +1,28 @@
+﻿namespace Hangman
+{
+    class HangmanGame
+    {
+        private bool _keepPlaying;
+        private int _guessesLeft;
+        private string _answer;
+        private int _lettersFound;
+
+        private string[] _guessedLetters;
+        private int _guessedLettersIndex;
+
+        private string[] _wordDisplay;
+
+        public void Play()
+        {
+            SetUp();
+
+            while (_keepPlaying)
+            {
+                DisplayPuzzle();
+                PrompUser();
+            }
+
+            DisplayResult();
+        }
+    }
+}
