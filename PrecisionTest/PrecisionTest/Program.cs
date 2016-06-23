6e9f24b8c866ea8222af5e081ce0eb41889f9e2b
Michael E Miles <michael@miles-me.net>
2016-06-23 14:11:35 -0500

    Precision demonstration between decimal and float

---
+++ PrecisionTest/PrecisionTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrecisionTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var seedF = 0.1f;
+            var seedD = 0.1m;
+            var valueF = 0f;
+            var valueD = 0m;
+
+            for (int i = 0; i < 10; i++)
+            {
+                valueF += seedF;
+                valueD += seedD;
+            }
+
+            Console.WriteLine($"ValueF: {1 - valueF}");
+            Console.WriteLine($"ValueD: {1 - valueD}");
+            Console.ReadLine();
+        }
+    }
+}
