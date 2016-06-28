0b35a24b182dcd7b763a83607edc9644e3f18417
Michael E Miles <michael@miles-me.net>
2016-06-28 09:02:11 -0500

    Replaced the declared delegate with Func<int, int,int>

--- SimpleDelegateExample/SimpleDelegateExample/Program.cs
+++ SimpleDelegateExample/SimpleDelegateExample/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace SimpleDelegateExample
+﻿using System;
+
+namespace SimpleDelegateExample
 {
     class Program
     {
+        //public delegate int MyDelegate(int a, int b);
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void CallDelegate(Func<int, int, int> myDelegate)
+        {
+            Console.WriteLine(myDelegate(272, 153));
+        }
+
         static void Main()
         {
+           Func<int, int, int> myDelegate = Add;
+
+            Console.WriteLine(myDelegate(271, 152));
+
+            CallDelegate(myDelegate);
+
+            Console.ReadLine();
 
         }
     }
