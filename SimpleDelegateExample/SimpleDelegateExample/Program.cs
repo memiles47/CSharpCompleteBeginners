21528f77a66f674429f185d1080625d55ff031a6
Michael E Miles <michael@miles-me.net>
2016-06-28 15:24:32 -0500

    tests

--- SimpleDelegateExample/SimpleDelegateExample/Program.cs
+++ SimpleDelegateExample/SimpleDelegateExample/Program.cs
@@ -13,19 +13,19 @@ namespace SimpleDelegateExample
 
         public static void CallDelegate(Func<int, int, int> myDelegate)
         {
-            Console.WriteLine(myDelegate(272, 153));
+            Console.WriteLine($"{myDelegate(272, 153)}\n");
         }
 
         static void Main()
         {
            Func<int, int, int> myDelegate = Add;
 
-            Console.WriteLine($"{myDelegate(271, 152)}\n");
+            Console.WriteLine($"{myDelegate(271, 152)}");
 
             CallDelegate(myDelegate);
 
             Func<string, int, int, string> ShowMe = (a, b, c) => string.Format(a, b, c, (b + c));
-            Console.WriteLine(ShowMe("{0} + {1} = {2}", 3, 5));
+            Console.WriteLine($"Result of calling the \'ShowMe\' Function: {ShowMe("{0} + {1} = {2}", 3, 5)}");
 
             Console.ReadLine();
 
